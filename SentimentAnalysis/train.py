9681acc127dc124c9c768c5ebc5dc5815a44cc4b
yujie <yujieguo0012@gmail.com>
2023-12-16 00:39:56 +0800

    modified train

--- SentimentAnalysis/train.py
+++ SentimentAnalysis/train.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sys
+import os
 sys.path.append('/root/ProductAnalyser/SentimentAnalysis')
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -13,13 +14,17 @@ from torch.optim import AdamW
 
 from dataset import ShoppingReviewDataset
 
-model_path = "/root/autodl-tmp/chinese_wwm"
-model_save_path = "/root/autodl-tmp/myChinese_wwm_ckps"
+model_path = "/root/autodl-tmp/chinese_wwm_pytorch"
+model_save_dir = "/root/autodl-tmp/myChinese_wwm_ckps"
 dataset_path = "/root/autodl-tmp/online_shopping_10_cats.csv"
+
+device = torch.device("cuda:0")
+
 df = pd.read_csv(dataset_path)
+df = df[df['cat'] == '手机']
 
 # 查看数据集结构
-# print(df.head())
+print(df.head())
 
 def merge_dictionaries(dic):
     merged_dict = {}
@@ -67,6 +72,7 @@ test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
 # 加载预训练的BERT模型
 model = BertForSequenceClassification.from_pretrained(model_path, num_labels=2)
+model = model.to(device)
 
 # 优化器
 optimizer = AdamW(model.parameters(), lr=1e-5)
@@ -74,15 +80,16 @@ optimizer = AdamW(model.parameters(), lr=1e-5)
 # 初始化用于保存loss值的列表
 epoch_loss_values = []
 
-for epoch in range(5):  # 迭代次数
+for epoch in range(3):  # 迭代次数
     model.train()
     total_loss = 0
+    avg_loss = 1e10
     # 使用tqdm显示进度条
-    for batch in tqdm(train_loader, desc=f"Epoch {epoch + 1}"):
+    for batch in tqdm(train_loader, desc=f"Epoch {epoch + 1}， loss: {avg_loss}"):
         optimizer.zero_grad()
-        input_ids = batch['input_ids']
-        attention_mask = batch['attention_mask']
-        labels = batch['labels']
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
         outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
         loss = outputs.loss
         total_loss += loss.item()
@@ -95,8 +102,9 @@ for epoch in range(5):  # 迭代次数
     with open('log.txt', 'a') as file:
         file.write(f"Epoch {epoch + 1} finished, Avg Loss: {avg_loss:.4f}")
     print(f"Epoch {epoch + 1} finished, Avg Loss: {avg_loss:.4f}")
-
-torch.save(model.state_dict(), model_save_path)
+    if not os.path.exists(model_save_dir):
+        os.makedirs(model_save_dir)
+    torch.save(model.state_dict(), os.path.join(model_save_dir, f"epoch_{epoch+1}.pth"))
 
 plt.plot(epoch_loss_values, label='Training Loss')
 plt.xlabel('Epochs')
